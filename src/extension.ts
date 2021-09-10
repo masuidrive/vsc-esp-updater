459dccb20ba6e7b266a5db67e52f0938ae2eae93
Yuichiro MASUI <masui@masuidrive.jp>
2021-09-10 15:33:22 +0000

    httpサーバを起動して起動してextension から開くのを確認

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+const http = require("http");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,13 +17,20 @@ export function activate(context: vscode.ExtensionContext) {
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "vsc-esp-updater.helloWorld",
-    () => {
+    async () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from vsc-esp-updater!");
+      // vscode.window.showInformationMessage("Hello World from vsc-esp-updater!");
+      http
+      .createServer((req: any, res: any) => {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Hello World\n");
+      })
+      .listen(3000, () => console.log("Server http://localhost:3000"));
       vscode.env.openExternal(
         vscode.Uri.parse(
-          "https://www.w3schools.com/python/python_functions.asp"
+          // "https://www.w3schools.com/python/python_functions.asp"
+          "http://localhost:3000/"
         )
       );
     }
