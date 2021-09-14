77074b14867f7c6597d2c4b805e4e231ea2f180b
Yuichiro Masui <masui@masuidrive.jp>
2021-09-14 17:23:51 +0900

    WIP

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -2,50 +2,50 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as http from "http";
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from "fs";
+import * as path from "path";
 
 const { window } = vscode;
 const port = 3232;
 
-let httpd:http.Server | null = null;
+let httpd: http.Server | null = null;
 
-function delay(duration:number): Promise<void> {
-  return new Promise(function(resolve, reject){
+function delay(duration: number): Promise<void> {
+  return new Promise(function (resolve, reject) {
     setTimeout(() => {
       resolve();
     }, duration);
   });
 }
 
-function getWorkspacePath():string | null {
+function getWorkspacePath(): string | null {
   return null;
 }
 
-let currentProjectPath:string | undefined = undefined;
-function startServer(projectPath:string): Promise<boolean> {
+let currentProjectPath: string | undefined = undefined;
+function startServer(projectPath: string): Promise<boolean> {
   currentProjectPath = projectPath;
-  return new Promise<boolean>(resolve => {
-    if(httpd !== null) {
+  return new Promise<boolean>((resolve) => {
+    if (httpd !== null) {
       resolve(false);
     }
     try {
       httpd = http.createServer((req: any, res: any) => {
-        if(req.url === '/') {
+        if (req.url === "/") {
           res.writeHead(200, { "Content-Type": "text/html" });
           // res.write(fs.readFileSync(path.resolve(__dirname, 'index.html')));
-          res.write(currentProjectPath ?? 'unknown');
-        }
-        else {
+          res.write(currentProjectPath ?? "unknown");
+          res.end();
+        } else {
           res.writeHead(404, { "Content-Type": "text/plain" });
           res.write("404 Not Found " + req.url);
+          res.end();
         }
       });
       httpd.listen(port, () => {
         resolve(true);
       });
-    }
-    catch(e) {
+    } catch (e) {
       httpd = null;
       resolve(false);
     }
@@ -53,77 +53,69 @@ function startServer(projectPath:string): Promise<boolean> {
 }
 
 function stopServer(): Promise<boolean> {
-  return new Promise<boolean>(resolve => {
-    if(httpd === null) {
+  return new Promise<boolean>((resolve) => {
+    if (httpd === null) {
       resolve(true);
     }
     try {
       httpd?.close();
       resolve(true);
-    }
-    catch(e) {
+    } catch (e) {
       resolve(false);
     }
   });
 }
 
-function findProjectPath(codePath:string, count = 0):string | undefined {
-  console.error('p', codePath);
-  if(codePath === undefined || codePath === '' || codePath === '/' || count > 30) { return undefined; }
+function findProjectPath(codePath: string, count = 0): string | undefined {
+  if (codePath === "" || codePath === "/" || count > 30) {
+    return undefined;
+  }
 
-  const builtFilePath = path.join(codePath, 'build', 'flasher_args.json');
-  if(fs.existsSync(builtFilePath)) {
+  const builtFilePath = path.join(codePath, "build", "flasher_args.json");
+  if (fs.existsSync(builtFilePath)) {
     return codePath;
-    }
-  const nextPath = path.dirname(codePath).split(path.sep).pop();
-  return findProjectPath(nextPath ?? '/', count + 1);
+  }
+  const nextPath = codePath.split(path.sep).slice(0, -1).join(path.sep);
+  return findProjectPath(nextPath ?? "/", count + 1);
 }
 
 let statusBarItem = window.createStatusBarItem();
 export function activate(context: vscode.ExtensionContext) {
   console.error("vsc-esp-updater.activate");
-  statusBarItem.text = 'Update　ESP';
-  statusBarItem.command = 'vsc-esp-updater.start';
+  statusBarItem.text = "Update ESP";
+  statusBarItem.command = "vsc-esp-updater.start";
   statusBarItem.show();
   context.subscriptions.push(statusBarItem);
-  // console.error(vscode.workspace.getWorkspaceFolder(vscode.window.activeTextEditor!.document!.uri)）;
 
   let disposableStart = vscode.commands.registerCommand(
     "vsc-esp-updater.start",
     async () => {
-      console.error("vsc-esp-updater.start");
-      const codeFilePath = vscode.window.activeTextEditor?.document?.uri?.fsPath;
-      console.error(codeFilePath);
-      if(codeFilePath === undefined) {
+      const codeFilePath =
+        vscode.window.activeTextEditor?.document?.uri?.fsPath;
+      if (codeFilePath === undefined) {
         vscode.window.showInformationMessage("Can't file active source code.");
         return;
       }
 
       const codePath = path.dirname(codeFilePath!);
       const projectPath = findProjectPath(codePath);
-      if(projectPath === undefined) {
+      if (projectPath === undefined) {
         vscode.window.showInformationMessage("Can't find built image file");
         return;
       }
-      console.error(projectPath);
 
-      if(projectPath !== currentProjectPath) {
+      if (projectPath !== currentProjectPath) {
         await stopServer();
         httpd = null;
       }
 
-      if(httpd === null) {
+      if (httpd === null) {
         await startServer(projectPath);
         currentProjectPath = projectPath;
-        await delay(1000);
+        await delay(500);
       }
 
-
-      vscode.env.openExternal(
-        vscode.Uri.parse(
-          `http://localhost:${port}`
-        )
-      );
+      vscode.env.openExternal(vscode.Uri.parse(`http://localhost:${port}`));
     }
   );
   context.subscriptions.push(disposableStart);
@@ -131,7 +123,7 @@ export function activate(context: vscode.ExtensionContext) {
   let disposableStop = vscode.commands.registerCommand(
     "vsc-esp-updater.stop",
     async () => {
-      if(await stopServer()) {
+      if (await stopServer()) {
         statusBarItem.hide();
       }
     }
