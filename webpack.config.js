ef134595adec98568e98db456b2ec37da9e88257
Yuichiro Masui <masui@masuidrive.jp>
2021-09-15 03:11:54 +0900

    Setting up

--- webpack.config.js
+++ webpack.config.js
@@ -74,6 +74,22 @@ const web_config = {
         use: [
           "style-loader",
           "css-loader",
+          {
+            // Run postcss actions
+            loader: 'postcss-loader',
+            options: {
+              // `postcssOptions` is needed for postcss 8.x;
+              // if you use postcss 7.x skip the key
+              postcssOptions: {
+                // postcss plugins, can be exported to postcss.config.js
+                plugins: function () {
+                  return [
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            },
+          },
           "sass-loader",
         ],
       },
