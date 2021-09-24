827848a14c15f0650d460c8ce74e7e5d04359a70
Yuichiro Masui <masui@masuidrive.jp>
2021-09-25 01:20:55 +0900

    WIP

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,85 +1,104 @@
 import { termInit, termClear, termWrite, termWriteln, term } from "./terminal";
-import { sleep } from "./utils";
+import { sleep, showEl, hideEl } from "./utils";
 import "./style.scss";
 import { EspLoader } from "@toit/esptool.js";
+import { Modal } from "bootstrap";
 
 export type Partition = {
   name: string;
   data: Uint8Array;
   offset: number;
 };
+let progressEl: HTMLElement | undefined;
+let navDisconnectedEl: HTMLElement | undefined;
+let navConnectedEl: HTMLElement | undefined;
+let navUpdatingEl: HTMLElement | undefined;
 
 let loader: EspLoader | undefined;
-
+const baudRate = 115200;
 async function doConnect() {
   let port = await navigator.serial.requestPort({
     // filters: [{ usbVendorId: 0x10c4 }],
   });
-  await port.open({ baudRate: 115200 });
+  await port.open({ baudRate: baudRate });
 
   loader = new EspLoader(port, { debug: true, logger: console });
-  console.log("connecting...");
   await loader.connect();
+
   try {
     await loader.sync();
-    console.log("connected");
 
     const chipName = await loader.chipName();
     const macAddr = await loader.macAddr();
     await loader.loadStub();
-    // await loader.setBaudRate(u, 115200);
-    //await loader.setBaudRate(options.baudRate, 921600);
+    await loader.setBaudRate(baudRate, 921600);
   } finally {
-    // await loader.disconnect();
   }
-  //}
 
-  console.log("done");
   return true;
 }
-async function doProgam() {
-  //async function doProgam() {
-  let project = await (
-    await fetch("project.json", {
-      cache: "no-cache",
-    })
-  ).json();
-
-  console.info(project);
-  const totalSize = project["files"].reduce(
-    (a: any, b: any) => a + b.size,
-    0
-  ) as number;
-  console.info(totalSize);
-
-  let wroteSize = 0;
-  for (let i in project["files"]) {
-    let file = project["files"][i];
-    const fileSize = file["size"];
-
-    let url = `data/${file["address"]}.bin`;
-    let contents = await (
-      await fetch(url, {
+async function doWrite() {
+  hideEl(navConnectedEl!);
+  showEl(navUpdatingEl!);
+  try {
+    let project = await (
+      await fetch("project.json", {
         cache: "no-cache",
       })
-    ).arrayBuffer();
-    await loader!.flashData(
-      new Uint8Array(contents),
-      parseInt(file["address"], 16),
-      (idx, cnt) => {
-        console.info(
-          "progress",
-          (wroteSize + (fileSize * idx) / cnt) / totalSize
-        );
-      }
-    );
-    wroteSize += fileSize;
-    await sleep(100);
+    ).json();
+
+    const totalSize = project["files"].reduce(
+      (a: any, b: any) => a + b.size,
+      0
+    ) as number;
+
+    let wroteSize = 0;
+    for (let i in project["files"]) {
+      let file = project["files"][i];
+      const fileSize = file["size"];
+
+      let url = `data/${file["address"]}.bin`;
+      let contents = await (
+        await fetch(url, {
+          cache: "no-cache",
+        })
+      ).arrayBuffer();
+      await loader!.flashData(
+        new Uint8Array(contents),
+        parseInt(file["address"], 16),
+        (idx, cnt) => {
+          const progress = Math.floor(
+            (100 * (wroteSize + (fileSize * idx) / cnt)) / totalSize
+          );
+          console.info("progress", progress);
+          progressEl!.style.width = `${progress}%`;
+        }
+      );
+      wroteSize += fileSize;
+      await sleep(100);
+    }
+  } finally {
+    hideEl(navUpdatingEl!);
+    showEl(navConnectedEl!);
   }
 }
 window.addEventListener("load", () => {
   termInit("terminal");
-  document.getElementById("btnConnect")!.addEventListener("click", doConnect);
-  // document.getElementById("btnErase")!.addEventListener("click", doErase);
-  document.getElementById("btnProgram")!.addEventListener("click", doProgam);
+  /*
+  progressEl = document.getElementById("progress")!;
+  navDisconnectedEl = document.getElementById("navDisconnected")!;
+  navConnectedEl = document.getElementById("navConnected")!;
+  navUpdatingEl = document.getElementById("navWriting")!;
+  */
+  let myModal = new Modal(document.getElementById("myModal")!);
+  document.getElementById("btnConnect")!.addEventListener("click", () => {
+    myModal.show();
+  });
+  document.getElementById("btnWrite")!.addEventListener("click", () => {});
+  document.getElementById("btnCancel")!.addEventListener("click", () => {});
+  // document.getElementById("btnDisconnect")!.addEventListener("click", () => {});
+  /*
+  showEl(navDisconnectedEl!);
+  hideEl(navConnectedEl!);
+  hideEl(navUpdatingEl!);*/
 });
--- src/web/utils.ts
+++ src/web/utils.ts
@@ -11,16 +11,10 @@ export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export function _arrayBufferToString(buffer: ArrayBuffer): string {
-  let tmp = [];
-  const len = 1024;
-  for (var p = 0; p < buffer.byteLength; p += len) {
-    tmp.push(
-      String.fromCharCode.apply(
-        "",
-        new Uint8Array(buffer.slice(p, p + len)) as any
-      )
-    );
-  }
-  return tmp.join("");
+export function showEl(el: HTMLElement) {
+  el.style.display = "block";
+}
+
+export function hideEl(el: HTMLElement) {
+  el.style.display = "none";
 }
